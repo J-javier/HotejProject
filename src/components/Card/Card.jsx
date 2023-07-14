c2ea09c03e47e3044236a10f7ec98d26dfdde60c
unknown <jose360pelico@gmail.com>
2023-07-13 19:17:02 -0600

    JSON file was added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,38 +1,64 @@
-import "./Card.css"
-import Star from "../../img/star-fill.svg"
+import "./Card.css";
+import Star from "../../img/star-fill.svg";
+
+import { useState, useEffect } from "react";
 
 function Card() {
+  const [data, setData] = useState([]);
+
+  // Función para traer los datos de "stays.json".
+  const getData = async () => {
+    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
+    try {
+      const res = await fetch("stays.json");
+      const resJson = await res.json();
+      // Aquí guardamos los datos de "stays.json" en la variable data.
+      setData(resJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="ContPrin">
-        <section className="SectionTitle">
-          <h2>Stays in Finland</h2>
-          <p>12+ stays</p>
-        </section>
-        <section className="card">
-          <div className="ContCard">
-            <div className="ContImg">
-              <img src="#" alt="Room Image" />
-            </div>
-            <div className="ContInf">
-              <div className="Information">
-                <span className="super">SUPER HOST</span>
-                <span>Entired apartment</span>
-                <span>2 beds</span>
-                <div>
-                  <img src={Star} alt="" />
+      <section className="SectionTitle">
+        <h2>Stays in Finland</h2>
+        <p>12+ stays</p>
+      </section>
+      <section className="card">
+        {data.map((inf, i) => {
+          return (
+            <>
+              <div key={i} className="ContCard">
+                <div className="ContImg">
+                  <img src={inf.photo} alt="Room Image" />
                 </div>
-                <span>4.50</span>
+                <div className="ContInf">
+                  <div className="Information">
+                    <span className="super">{inf.superHost}</span>
+                    <span className="apartment">{inf.type}</span>
+                    <span className="apartment">{inf.beds} beds</span>
+                    <div className="recomendacion">
+                      <img src={Star} alt="Rate" />
+                      <span className="rate">{inf.rating}</span>
+                    </div>
+                  </div>
+                  <p className="parrafo">
+                   {inf.title}
+                  </p>
+                </div>
               </div>
-              <p>Stylist apartment in center of the city</p>
-            </div>
-          </div>
-        </section>
-
-
-
-        
+            </>
+          );
+        })}
+      </section>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
